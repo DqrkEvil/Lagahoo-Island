a20ee6e2b56e2771d06becb2400a3d6b4664d88c
Algaras-dev <145165373+Algaras-dev@users.noreply.github.com>
2023-12-11 19:55:56 +0100

    Added variables for tile descriptions when creating map and put in placeholders

---
+++ directions.py
@@ -0,0 +1,6 @@
+'''Fil för standardiserade riktningar'''
+
+forward = 'framåt'
+downward = 'bakåt'
+right = 'till höger'
+left = 'till vänster'
--- level.py
+++ level.py
@@ -1,18 +1,27 @@
 '''Tile klasser'''
 
 import items
+import directions
 
 # Classes
 class Tile():
     '''Basklassen för alla tiles på kartan'''
 
     def __init__(self,
+                 tile_description: tuple[str, str],
                  avalible_directions: tuple,
                  useable_items: tuple = tuple(),
                  actions: tuple = tuple(),
                  findable_items: tuple = tuple()
                  ) -> None:
+        '''
+        :tile_description: tuple med beskrivning av denna tilen som skrivs ut när spelaren kommer hit och ev. beskrivningen av tiles man kan gå till
+        :avalible_directions: alla vägar att gå från denna tile
+        :useable_items: items som går att använda på denna tile
+        :actions: alla möjliga aktiviteter på denna tile
+        :findable_items: saker som går att ta från denna tile'''
 
+        self.tile_description = tuple(tile_description)
         self.avalible_directions = tuple(avalible_directions)
         self.useable_items = tuple(useable_items)
         self.findable_items = tuple(findable_items)
@@ -22,12 +31,20 @@ class Tile():
 
 class CavePuzzleTile(Tile):
     '''Subklass för pussel tiles i grottan'''
-    def __init__(self,
-                 avalible_directions: tuple,
-                 useable_items: tuple = tuple()
-                 ) -> None:
 
-        super().__init__(avalible_directions, useable_items)
+    def __init__(self, tile_description: tuple[str, str],
+                 avalible_directions: tuple,
+                 useable_items: tuple = tuple(),
+                 actions: tuple = tuple(),
+                 findable_items: tuple = tuple()) -> None:
+        '''
+        :tile_description: tuple med beskrivning av denna tilen som skrivs ut när spelaren kommer hit och ev. beskrivningen av tiles man kan gå till
+        :avalible_directions: alla vägar att gå från denna tile
+        :useable_items: items som går att använda på denna tile
+        :actions: alla möjliga aktiviteter på denna tile
+        :findable_items: saker som går att ta från denna tile'''
+
+        super().__init__(tile_description, avalible_directions, useable_items, actions, findable_items)
 
         self.is_torch_lit = False
 
@@ -45,26 +62,55 @@ class level():
     def create_level(self):
         '''Skapa kartan för spelet'''
 
-        hall = Tile(('right', 'down'))
-        kitchen = Tile(('left'))
+        hall = Tile(('Du är i hallen av stugan', 'Du ser en stuga %(direction)s'),
+                    (directions.right, directions.downward))
 
-        turtle = Tile(('down'), (items.golden_seaweed))
-        cottege_entrance = Tile(('up', 'down'))
-        lake = Tile(('right', 'down'))
-        parrot = Tile(('left'), actions='Prata med papegoja')
+        kitchen = Tile(('Dur är i stugans kök', 'Det finns ett kök %(direction)s'),
+                       (directions.left),
+                       findable_items=(items.torch))
 
-        base = Tile(('up', 'down', 'right'))
-        jungle1 = Tile(('up', 'down', 'right', 'left'))
-        monkey = Tile(('up', 'left'), ('hatchet'), ('Prata med apan'))
+        # Ny rad
+        turtle = Tile(('Du står på stranden, nära dig ligger en sköldpadda', '%(direction)s ligger en sköldpadda'),
+                      (directions.downward),
+                      (items.golden_seaweed))
 
-        beach = Tile(('up'), (items.shovel))
-        mountain = Tile(('up', 'right'))
-        cave_entrance = Tile(('down', 'right', 'left'))
-        cave_1 = CavePuzzleTile(('left'), (items.torch))
+        cottege_entrance = Tile(('Du står i jungeln bredvid en liten stuga, dörren står lite på glänt', 'Det finns en stuga %(direction)s om dig'),
+                                (directions.forward, directions.downward))
 
-        cave_2 = CavePuzzleTile(('up'), (items.torch))
-        hidden_cave = Tile(('up', 'right'), findable_items=items.hatchet)
+        lake = Tile(('beskrivning', 'vad man ser från närliggande tile'),
+                    (directions.right, directions.downward))
+        parrot =            Tile('', ('left'), actions='Prata med papegoja')
+
+        # Ny rad
+        base = Tile(('beskrivning', 'vad man ser från närliggande tile'),
+                    (directions.forward, directions.downward, directions.right))
+
+        jungle1 = Tile(('beskrivning', 'vad man ser från närliggande tile'),
+                       (directions.forward, directions.downward, directions.right, 'left'))
+
+        monkey = Tile(('beskrivning', 'vad man ser från närliggande tile'),
+                      (directions.forward, 'left'), ('hatchet'), ('Prata med apan'))
+
+        # Ny rad
+        beach = Tile(('beskrivning', 'vad man ser från närliggande tile'),
+                     (directions.forward), (items.shovel))
+
+        mountain = Tile(('beskrivning', 'vad man ser från närliggande tile'),
+                        (directions.forward, directions.right))
+
+        cave_entrance = Tile(('beskrivning', 'vad man ser från närliggande tile'),
+                             (directions.downward, directions.right, 'left'))
+
+        cave_1 = CavePuzzleTile(('beskrivning', 'vad man ser från närliggande tile'),
+                                ('left'), (items.torch))
         
+        # Ny rad
+        cave_2 = CavePuzzleTile(('beskrivning', 'vad man ser från närliggande tile'),
+                                (directions.forward), (items.torch))
+
+        hidden_cave = Tile(('beskrivning', 'vad man ser från närliggande tile'),
+                           (directions.forward, directions.right), findable_items=items.hatchet)
+
         self.level = (
             (None, hall, kitchen, None),
             (turtle, cottege_entrance, lake, parrot),

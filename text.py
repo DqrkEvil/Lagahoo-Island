8359ade0e18cdb0ad6e14fab8103830ab7090005
Algaras-dev <145165373+Algaras-dev@users.noreply.github.com>
2023-12-14 13:23:35 +0100

    Moved big text blocks into new file

---
+++ text.py
@@ -0,0 +1,51 @@
+mainMenuText = r"""
+
+
+
+
+                            ┌───────────────────────────────────────────────────────────────────────────────┐
+                            │                                                                               │
+                            │  __          ___       _______      ___       __    __    ______     ______   │
+                            │ |  |        /   \     /  _____|    /   \     |  |  |  |  /  __  \   /  __  \  │
+                            │ |  |       /  ^  \   |  |  __     /  ^  \    |  |__|  | |  |  |  | |  |  |  | │
+                            │ |  |      /  /_\  \  |  | |_ |   /  /_\  \   |   __   | |  |  |  | |  |  |  | │
+                            │ |  `----./  _____  \ |  |__| |  /  _____  \  |  |  |  | |  `--'  | |  `--'  | │
+                            │ |_______/__/     \__\ \______| /__/     \__\ |__|  |__|  \______/   \______/  │
+                            │                                                                               │
+                            │          __       _______. __          ___      .__   __.  _______            │
+                            │         |  |     /       ||  |        /   \     |  \ |  | |       \           │
+                            │         |  |    |   (----`|  |       /  ^  \    |   \|  | |  .--.  |          │
+                            │         |  |     \   \    |  |      /  /_\  \   |  . `  | |  |  |  |          │
+                            │         |  | .----)   |   |  `----./  _____  \  |  |\   | |  '--'  |          │
+                            │         |__| |_______/    |_______/__/     \__\ |__| \__| |_______/           │
+                            │                                                                               │
+                            └───────────────────────────────────────────────────────────────────────────────┘
+
+                            
+"""
+
+startGameText = "                                                      Press enter to start game"
+
+preGameLore = """
+Du ligger på den fuktiga sanden, den tropiska luften fyller dina lungor medans du stirrar upp i det djupt mörkblåa eviga tomrum fullt av stjärnor. 
+**Insert ascii art of stars**
+**Insert sound of waves washing up on the shore**
+Tankarna väller fram; 
+Vad hände med de andra, är det saken som vi skulle undersöka som är anledningen till att de har försvunnit? 
+Hur ska jag komma tillbaka till fastlandet?
+Är jag den enda överlevaren?
+Desto mer du ligger och tänker desto tyngre blir ögonlocken.
+Till slut blir det svart"""
+
+controlInfo = """
+Information on kontroller:
+
+Använd WASD / piltangenterna för att gå runt
+
+i: information
+
+m: öppna kartan
+
+e: använd föremål
+
+q: plocka upp föremål i nuvarande rum"""

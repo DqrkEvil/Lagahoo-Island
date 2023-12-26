865f06e268d06b1f82a56978102d66e34a6a77dc
Algaras <145165373+Algaras-dev@users.noreply.github.com>
2023-12-26 20:05:31 +0100

    Added delay to input loop (now less resource intensive to run)

--- IO.py
+++ IO.py
@@ -18,46 +18,49 @@ class Controls():
     def __init__(self) -> None:
         self.add_hotkeys()
 
-        self.last_key = None
+        self.key = None
 
     def await_input(self) -> str:
         '''Vänta tills användaren ger en input'''
 
         # Återställ variabeln
-        self.last_key = None
+        self.key = None
 
         # Vänta tills en annan knapp trycks ner
         while True:
-            if self.last_key is not None:
-                return self.last_key
+            if self.key is not None:
+                return self.key
 
-    def change_key(self, key: str) -> None:
+            # Sakta ner loopen för att använda mindre resurser
+            time.sleep(0.1)
+
+    def key_pressed(self, key: str) -> None:
         '''Uppdatera variablen med den senaste nedtryckta knappen'''
 
-        self.last_key = key
+        self.key = key
 
     def add_hotkeys(self) -> None:
         '''Lägg till hotkeys för att kontrollera'''
 
-        keyboard.add_hotkey("w", lambda: self.change_key(directions.right))
-        keyboard.add_hotkey("up arrow", lambda: self.change_key(directions.right))
+        keyboard.add_hotkey("w", lambda: self.key_pressed(directions.right))
+        keyboard.add_hotkey("up arrow", lambda: self.key_pressed(directions.right))
 
-        keyboard.add_hotkey("s", lambda: self.change_key(directions.left))
-        keyboard.add_hotkey("down arrow", lambda: self.change_key(directions.left))
+        keyboard.add_hotkey("s", lambda: self.key_pressed(directions.left))
+        keyboard.add_hotkey("down arrow", lambda: self.key_pressed(directions.left))
 
-        keyboard.add_hotkey("d", lambda: self.change_key(directions.down))
-        keyboard.add_hotkey("right arrow", lambda: self.change_key(directions.down))
+        keyboard.add_hotkey("d", lambda: self.key_pressed(directions.down))
+        keyboard.add_hotkey("right arrow", lambda: self.key_pressed(directions.down))
 
-        keyboard.add_hotkey("a", lambda: self.change_key(directions.up))
-        keyboard.add_hotkey("left arrow", lambda: self.change_key(directions.up))
+        keyboard.add_hotkey("a", lambda: self.key_pressed(directions.up))
+        keyboard.add_hotkey("left arrow", lambda: self.key_pressed(directions.up))
 
-        keyboard.add_hotkey("i", lambda: self.change_key("info"))
+        keyboard.add_hotkey("i", lambda: self.key_pressed("info"))
 
-        keyboard.add_hotkey("m", lambda: self.change_key("map"))
+        keyboard.add_hotkey("m", lambda: self.key_pressed("map"))
 
-        keyboard.add_hotkey("e", lambda: self.change_key("use item"))
+        keyboard.add_hotkey("e", lambda: self.key_pressed("use item"))
 
-        keyboard.add_hotkey("q", lambda: self.change_key("pickup"))
+        keyboard.add_hotkey("q", lambda: self.key_pressed("pickup"))
 
 def integer_input(acceptable_values: tuple | None = None) -> int:
     '''Tar en input från avnändaren som måste vara en int'''
